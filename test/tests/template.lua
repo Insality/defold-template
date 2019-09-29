b9d698e5c244cab3e8bf8f8c91eaa8032b1cb942
Insality <insality@gmail.com>
2019-09-30 01:21:26 +0300

    initial template commit

---
+++ test/tests/template.lua
@@ -0,0 +1,16 @@
+return function()
+	describe("Section", function()
+		before(function()
+		end)
+
+		after(function()
+		end)
+
+		it("Test1", function()
+		end)
+
+
+		it("Test2", function()
+		end)
+	end)
+end
